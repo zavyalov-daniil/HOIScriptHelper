cd8d6f13ad7d3dc1cb5eb0e1e5c100c81c71ebb7
Daniil Zavyalov <dezavv1@gmail.com>
2023-02-12 22:26:12 +0600

    fix

---
+++ src/main/java/com/dev/hoiscripthelper/Controller.java
@@ -0,0 +1,25 @@
+package com.dev.hoiscripthelper;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private Button chooseTamplateButton;
+
+    @FXML
+    private Button enterCodeButton;
+
+    @FXML
+    private Button enterKeyButton;
+
+    @FXML
+    private Button readTagsButton;
+
+    @FXML
+    private Label welcomeText;
+
+}
+
